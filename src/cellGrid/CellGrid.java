fd37cf85041fee14378a40cd23558c912e2644be
Jonathan Rub <jonathan.rub@duke.edu>
2017-01-31 17:49:38 -0500

    used the cellGridNeighbors class to get the neighbors for the cell grid

--- src/cellGrid/CellGrid.java
+++ src/cellGrid/CellGrid.java
@@ -4,74 +4,31 @@ import java.util.*;
 
 import cell.Cell;
 
-public class CellGrid
-{
+public class CellGrid {
 	private Cell[][] myGrid;
+	private int myHieght;
+	private int myWidth;
 
-	public CellGrid(int size)
-	{
+	public CellGrid(int size) {
 		myGrid = new Cell[size][size];
+		myHieght = size;
+		myWidth = size;
 	}
 
-	public Cell[][] getGrid()
-	{
+	public Cell[][] getGrid() {
 		return myGrid;
 	}
 
-	public Map getNeighbors(int x, int y){
-		Map neighbors = new HashMap();
-		if (myGrid[x][y].getIsEdge){
-			if (){
-				
-			}else if(){
-				
-			}else if(){
-				
-			}
-		}else{
-			putAll(neighbors);
-		}
+	public int getHieght() {
+		return myHieght;
 	}
 
-	private void putAll(Map neighbors)
-	{
-		putBottom(neighbors);
-		putTop(neighbors);
+	public int getWidth() {
+		return myWidth;
 	}
-
-	private void putBottom(Map neighbors){
-		neighbors.put("Center Left", myGrid[x-1][y])
-		neighbors.put("Center", myGrid[x][y])
-		neighbors.put("Center Right", myGrid[x+1][y])
-		neighbors.put("Bottom Left", myGrid[x-1][y+1])
-		neighbors.put("Bottom", myGrid[x][y+1])
-		neighbors.put("Bottom Right", myGrid[x+1][y+1])
-	}
-
-	private void putTop(Map neighbors){
-		neighbors.put("Top Left", myGrid[x-1][y-1])
-		neighbors.put("Top", myGrid[x][y-1])
-		neighbors.put("Top Right", myGrid[x+1][y-1])
-		neighbors.put("Center Left", myGrid[x-1][y])
-		neighbors.put("Center", myGrid[x][y])
-		neighbors.put("Center Right", myGrid[x+1][y])
-	}
-
-	private void putRight(Map neighbors){
-		neighbors.put("Top", myGrid[x][y-1])
-		neighbors.put("Top Right", myGrid[x+1][y-1])
-		neighbors.put("Center", myGrid[x][y])
-		neighbors.put("Center Right", myGrid[x+1][y])
-		neighbors.put("Bottom", myGrid[x][y+1])
-		neighbors.put("Bottom Right", myGrid[x+1][y+1])
-	}
-
-	private void putLeft(Map neighbors){
-		neighbors.put("Top Left", myGrid[x-1][y-1])
-		neighbors.put("Top", myGrid[x][y-1])
-		neighbors.put("Center Left", myGrid[x-1][y])
-		neighbors.put("Center", myGrid[x][y])
-		neighbors.put("Bottom Left", myGrid[x-1][y+1])
-		neighbors.put("Bottom", myGrid[x][y+1])
+	
+	public Map<String, Cell> getNeighbors(int x, int y) {
+		cellGridNeighbors cell = new cellGridNeighbors(myGrid, x, y);
+		return cell.getNeighbors();
 	}
 }
