4a756ff78274ada22fbaaded75b73b6286a68086
Jimmy Shackford <jas199@duke.edu>
2017-01-31 16:31:57 -0500

    Added Display.java (hopefully now works) which will display the cell grid according to cell states

--- src/Display/Display.java
+++ src/Display/Display.java
@@ -4,29 +4,67 @@ import java.util.ArrayList;
 
 import cell.Cell;
 import cellGrid.CellGrid;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
 import javafx.scene.shape.Rectangle;
+import javafx.scene.shape.Shape;
+import javafx.stage.Stage;
 
 public class Display
 {
-	public final double DISPLAY_HEIGHT = 600;
-	public final double DISPLAY_WIDTH = 600;
+	public static final double DISPLAY_HEIGHT = 600;
+	public static final double DISPLAY_WIDTH = 600;
+	public static final Paint BACKGROUND_COLOR = Color.BLACK;
 
-	public void displayGrid(CellGrid cellGrid)
+	private Stage stage;
+	private Group root;
+	private Scene scene;
+	private CellGrid cellGrid;
+
+	public Display(Stage stage)
+	{
+		this.stage = stage;
+
+	}
+
+	public void setupScene()
+	{
+		root = new Group();
+		scene = new Scene(root, DISPLAY_WIDTH, DISPLAY_HEIGHT, BACKGROUND_COLOR);
+	}
+
+	public void refreshDisplay()
+	{
+		root.getChildren().clear();
+	}
+
+	public void displayGrid()
 	{
 		int height = cellGrid.getHeight();
 		int width = cellGrid.getWidth();
 		int cellHeight = DISPLAY_HEIGHT / cellGrid.getHeight();
 		int cellWidth = DISPLAY_WIDTH / cellGrid.getWidth();
 
-		ArrayList<Rectangle> addedPictures = new ArrayList<Rectangle>();
+		ArrayList<Shape> addedCells = new ArrayList<Shape>();
 
 		for (int i = 0; i < cellGrid.getGrid().length; i++) {
 			for (int j = 0; j < cellGrid.getGrid()[i].length; j++) {
 				Cell currCell = cellGrid.getGrid()[i][j];
 				Rectangle newCell = new Rectangle(cellWidth * j, cellHeight * i, j, i);
 				newCell.setFill(cellGrid.getStateMap().get(currCell.getState()));
-				addedPictures.add(newCell);
+				addedCells.add(newCell);
 			}
+		}
+		refreshDisplay();
+		displayShapes(addedCells);
+	}
+
+	public void displayShapes(ArrayList<Shape> shapes)
+	{
+		for (Shape shape : shapes) {
+			root.getChildren().add(shape);
 		}
 	}
 }
