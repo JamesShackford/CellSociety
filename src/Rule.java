46b3a9f4bc9aacedc153d045e94df3509084b205
Jimmy Shackford <jas199@duke.edu>
2017-02-01 23:02:16 -0500

    Updated Rule to contain cell grid.

--- src/Rule.java
+++ src/Rule.java
@@ -1,11 +1,29 @@
 import java.util.Map;
 
+import cellGrid.CellGrid;
 import javafx.scene.control.Cell;
 import javafx.scene.paint.Paint;
 
-public interface Rule
+public abstract class Rule
 {
+	CellGrid cellGrid;
+
+	public Rule(CellGrid cellGrid)
+	{
+		this.cellGrid = cellGrid;
+	}
+
 	public Map<Integer, Paint> getStateMap();
 
 	public int getNextState(Cell cell);
+
+	public CellGrid getCellGrid()
+	{
+		return cellGrid;
+	}
+
+	public void setCellGrid(CellGrid cellGrid)
+	{
+		this.cellGrid = cellGrid;
+	}
 }
