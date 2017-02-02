bf05a69a2beb48f47186aa68f6a78c58a537972a
Jonathan Rub <jonathan.rub@duke.edu>
2017-02-02 16:14:30 -0500

    added fire simulation and made sure it worked

--- src/cellsociety/CellSociety.java
+++ src/cellsociety/CellSociety.java
@@ -3,6 +3,7 @@ package cellsociety;
 import java.util.Random;
 
 import Display.Display;
+import Rule.FireRule;
 import Rule.GameOfLifeRule;
 import Rule.Rule;
 import cell.Cell;
@@ -18,17 +19,33 @@ public class CellSociety
 	public CellSociety()
 	{
 		cellGrid = new CellGrid(GRID_SIZE);
-		rule = new GameOfLifeRule(cellGrid);
+//		rule = new GameOfLifeRule(cellGrid);
+//		for (int i = 0; i < cellGrid.getGrid().length; i++) {
+//			for (int j = 0; j < cellGrid.getGrid()[i].length; j++) {
+//				Random rand = new Random();
+//				if (rand.nextDouble() < 0.5) {
+//					cellGrid.getGrid()[i][j] = new Cell(rule, 1, i, j);
+//				} else {
+//					cellGrid.getGrid()[i][j] = new Cell(rule, 0, i, j);
+//				}
+//			}
+//		}
+		
+		rule = new FireRule(cellGrid, 0.5);
 		for (int i = 0; i < cellGrid.getGrid().length; i++) {
 			for (int j = 0; j < cellGrid.getGrid()[i].length; j++) {
 				Random rand = new Random();
-				if (rand.nextDouble() < 0.5) {
+				double c = rand.nextDouble();
+				if (c < 0.2) {
+					cellGrid.getGrid()[i][j] = new Cell(rule, 2, i, j);
+				}else if (c < 0.90){
 					cellGrid.getGrid()[i][j] = new Cell(rule, 1, i, j);
-				} else {
+				}else{
 					cellGrid.getGrid()[i][j] = new Cell(rule, 0, i, j);
 				}
 			}
 		}
+		
 		// cellGrid.getGrid()[3][3] = new Cell(rule, 1, 3, 3);
 		// cellGrid.getGrid()[4][3] = new Cell(rule, 1, 4, 3);
 		// cellGrid.getGrid()[3][4] = new Cell(rule, 1, 3, 4);
