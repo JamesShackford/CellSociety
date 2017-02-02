5f407217938abc2472f27f49171f444aaaa931ff
Derek Hill <dsh38@duke.edu>
2017-02-02 00:26:01 -0500

    cleaned up life and predprey rules

--- src/Rule/PredatorPreyRule.java
+++ src/Rule/PredatorPreyRule.java
@@ -12,14 +12,17 @@ public class PredatorPreyRule extends Rule {
 	public static final int EMPTY = 0;
 	public static final int FISH = 1;
 	public static final int SHARK = 2;
+	public static final Paint EMPTY_COLOR = Color.BLUE;
+	public static final Paint FISH_COLOR = Color.SALMON;
+	public static final Paint SHARK_COLOR = Color.GRAY;
 	private Map<Integer, Paint> stateColorMap;
 
 	public PredatorPreyRule(CellGrid cellGrid) {
 		super(cellGrid);
 		stateColorMap = new HashMap<Integer, Paint>();
-		stateColorMap.put(EMPTY, Color.BLUE);
-		stateColorMap.put(FISH, Color.SALMON);
-		stateColorMap.put(SHARK, Color.GRAY);
+		stateColorMap.put(EMPTY, EMPTY_COLOR);
+		stateColorMap.put(FISH, FISH_COLOR);
+		stateColorMap.put(SHARK, SHARK_COLOR);
 	}
 
 	@Override
@@ -30,31 +33,11 @@ public class PredatorPreyRule extends Rule {
 	@Override
 	public int getNextState(Cell cell) {
 		Map<String, Cell> map = getCellGrid().getNeighborsWrap(cell.getX(), cell.getY());
-		if (cell.getState() == FISH){
-			
-		for (Cell neighborCell : map.values()) {
-			if (neighborCell != null) {
-				if (neighborCell.getState() == 1) {
-					numAlive++;
-				}
-			}
-
+		if (cell.getState() == FISH) {
+			return FISH;
 		}
-
-		// need to simplify this if statement structure
-		if (cell.getState() == 1) {
-			if (numAlive < 2 || numAlive > 3) {
-				return DEAD;
-			} else if (numAlive == 2 || numAlive == 3) {
-				return ALIVE;
-			}
-
-		} else if (numAlive == 3) {
-			return ALIVE;
-		}
-		return cell.getState();
-
-	}
+		return EMPTY;
+		
 	}
 
 }
