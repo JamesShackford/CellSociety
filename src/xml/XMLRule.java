660bda6fdb90fc4aeba456841a1fe67b62eb5379
Jimmy Shackford <jas199@duke.edu>
2017-02-07 02:24:54 -0500

    Allow xml files to specify initial conditions.

--- src/xml/XMLRule.java
+++ src/xml/XMLRule.java
@@ -1,10 +1,37 @@
 package xml;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
 
+import cell.Cell;
+import cell.RectangularCell;
+import cellgrid.CellGrid;
 import rule.Rule;
 
-public interface XMLRule
+public abstract class XMLRule
 {
-	public Rule makeRule(Map<String, String> stringVars);
+	public static final List<String> DATA_FIELDS = Arrays.asList(new String[] { "width", "height", "initial_states" });
+
+	public Rule makeRule(Map<String, String> dataValues)
+	{
+		Rule myRule = getRule(dataValues);
+		int width = Integer.parseInt(dataValues.get(DATA_FIELDS.get(0)));
+		int height = Integer.parseInt(dataValues.get(DATA_FIELDS.get(1)));
+		String[] initialValues = dataValues.get(DATA_FIELDS.get(2)).split("\n");
+
+		CellGrid myGrid = new CellGrid(width, height);
+		myRule.setCellGrid(myGrid);
+
+		for (int i = 0; i < height; i++) {
+			for (int j = 0; j < width; j++) {
+				int currState = Integer.parseInt(initialValues[i].split(" ")[j]);
+				Cell addedCell = new RectangularCell(myRule, currState, i, j);
+				myGrid.setCell(i, j, addedCell);
+			}
+		}
+		return myRule;
+	}
+
+	public abstract Rule getRule(Map<String, String> dataValues);
 }
