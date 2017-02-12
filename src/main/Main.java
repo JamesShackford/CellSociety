1dab37a9c93cd7ab4c582339b6f5ef776d336223
Jimmy Shackford <jas199@duke.edu>
2017-02-12 17:30:29 -0500

    Updated gui to look nice, step button works.

--- src/main/Main.java
+++ src/main/Main.java
@@ -30,46 +30,8 @@ public class Main extends Application
 		List<CellSociety> cellSocieties = new ArrayList<CellSociety>();
 		cellSocieties.add(cellSociety);
 
-		ruleName = (String) XMLParser.RULE_MAP.keySet().toArray()[1];
-		rule = new XMLParser().getRule(XMLParser.FILE_MAP.get(ruleName));
-		cellSociety = new CellSociety(rule);
-		cellSocieties.add(cellSociety);
-
-		ruleName = (String) XMLParser.RULE_MAP.keySet().toArray()[2];
-		rule = new XMLParser().getRule(XMLParser.FILE_MAP.get(ruleName));
-		cellSociety = new CellSociety(rule);
-		cellSocieties.add(cellSociety);
-
 		display = new Display(stage, cellSocieties);
 	}
-
-	// private void handleKeyReleased(KeyCode code) throws Exception
-	// {
-	// if (code == KeyCode.S) {
-	// myAnimation.stop();
-	// }
-	// if (code == KeyCode.P) {
-	// myAnimation.pause();
-	// }
-	// if (code == KeyCode.B) {
-	// myAnimation.play();
-	// }
-	// if (code == KeyCode.F) {
-	// myAnimation.setRate(myAnimation.getCurrentRate() + 0.25);
-	// }
-	// if (code == KeyCode.D) {
-	// if (myAnimation.getCurrentRate() > 0) {
-	// myAnimation.setRate(myAnimation.getCurrentRate() - 0.25);
-	// }
-	// }
-	// if (code == KeyCode.Q) {
-	// // step(SECOND_DELAY);
-	// }
-	// if (code == KeyCode.N) {
-	// myAnimation.stop();
-	// start(myStage);
-	// }
-	// }
 
 	public static void main(String[] args)
 	{
