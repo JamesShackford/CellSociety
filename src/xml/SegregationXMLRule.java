40da8f284177875dc1833f3c80eab40e54a1e1a6
Jonathan Rub <jonathan.rub@duke.edu>
2017-02-12 19:59:56 -0500

    added exception handling

--- src/xml/SegregationXMLRule.java
+++ src/xml/SegregationXMLRule.java
@@ -7,22 +7,28 @@ import java.util.Map;
 import rule.Rule;
 import rule.SegregationRule;
 
-public class SegregationXMLRule extends XMLRule
-{
+public class SegregationXMLRule extends XMLRule {
 
 	public static final String DATA_TYPE = "Segregation";
 
 	public static final List<String> DATA_FIELDS = makeDataFields();
 
 	@Override
-	public Rule getRule(Map<String, String> dataValues)
-	{
-		double threshold = Double.parseDouble(dataValues.get(DATA_FIELDS.get(0)));
+	public Rule getRule(Map<String, String> dataValues) {
+		double threshold = 0.0;
+		Exception invalidThresholdException = new Exception();
+		try {
+			threshold = Double.parseDouble(dataValues.get(DATA_FIELDS.get(0)));
+			if (threshold < 0 || threshold > 1) {
+				throw invalidThresholdException;
+			}
+		} catch (Exception e) {
+			alert("INVALIDTHRESHOLD");
+		}
 		return new SegregationRule(threshold);
 	}
 
-	private static List<String> makeDataFields()
-	{
+	private static List<String> makeDataFields() {
 		List<String> dataFields = new ArrayList<String>();
 		dataFields.add("threshold");
 		dataFields.addAll(XMLRule.DATA_FIELDS);
